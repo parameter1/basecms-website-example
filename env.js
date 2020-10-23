fcbabd13af2046948c4f7b3d55cad3e0409222d0
Josh Worden <josh@parameter1.com>
2020-10-23 12:55:24 -0500

    Set default env vars

--- env.js
+++ env.js
@@ -1,11 +1,19 @@
 const { cleanEnv, validators } = require('@base-cms/env');
 
 const { nonemptystr } = validators;
+const toApply = ['TENANT_KEY', 'SITE_ID', 'OEMBED_URI', 'GRAPHQL_URI', 'RSS_URI', 'SITEMAPS_URI'];
 
-module.exports = () => cleanEnv(process.env, {
-  TENANT_KEY: nonemptystr({ desc: 'The BaseCMS tenant key.' }),
-  OEMBED_URI: nonemptystr({ desc: 'The BaseCMS OEmbed URI.' }),
-  GRAPHQL_URI: nonemptystr({ desc: 'The BaseCMS Graph URI.' }),
-  RSS_URI: nonemptystr({ desc: 'The BaseCMS RSS URI.' }),
-  SITEMAPS_URI: nonemptystr({ desc: 'The BaseCMS Sitemap URI.' }),
-});
+module.exports = () => {
+  const envs = cleanEnv(process.env, {
+    TENANT_KEY: nonemptystr({ desc: 'The BaseCMS tenant key.', default: 'p1_sandbox' }),
+    OEMBED_URI: nonemptystr({ desc: 'The BaseCMS OEmbed URI.', default: 'https://tauron.oembed.base.parameter1.com' }),
+    GRAPHQL_URI: nonemptystr({ desc: 'The BaseCMS Graph URI.', default: 'https://tauron.graphql.base.parameter1.com' }),
+    RSS_URI: nonemptystr({ desc: 'The BaseCMS RSS URI.', default: 'https://tauron.rss.base.parameter1.com' }),
+    SITEMAPS_URI: nonemptystr({ desc: 'The BaseCMS Sitemap URI.', default: 'https://tauron.sitemaps.base.parameter1.com' }),
+    SITE_ID: nonemptystr({ desc: 'The BaseCMS site id.', default: '5ed294c6c13a4626008b4568' }),
+  });
+  // Set the values back to the process' environment variables
+  // eslint-disable-next-line no-return-assign
+  toApply.forEach((k) => process.env[k] = envs[k]);
+  return envs;
+};
